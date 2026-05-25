7ffa4ac095930ce4a0da230125ad82c397b227d7
Dera Okeke <okekechidera97@gmail.com>
2026-05-25 14:56:44 +0100

    chore: lint format

--- app/components/tabs.tsx
+++ app/components/tabs.tsx
@@ -1,6 +1,6 @@
-'use client';
+'use client'
 
-import * as React from 'react';
+import * as React from 'react'
 import {
   type ComponentProps,
   createContext,
@@ -10,11 +10,11 @@ import {
   useId,
   useMemo,
   useState,
-} from 'react';
-import { cn } from '../lib/cn';
-import * as Unstyled from './ui/tabs';
+} from 'react'
+import { cn } from '../lib/cn'
+import * as Unstyled from './ui/tabs'
 
-type CollectionKey = string | symbol;
+type CollectionKey = string | symbol
 
 export interface TabsProps extends Omit<
   ComponentProps<typeof Unstyled.Tabs>,
@@ -23,30 +23,30 @@ export interface TabsProps extends Omit<
   /**
    * Use simple mode instead of advanced usage as documented in https://radix-ui.com/primitives/docs/components/tabs.
    */
-  items?: string[];
+  items?: string[]
 
   /**
    * Shortcut for `defaultValue` when `items` is provided.
    *
    * @defaultValue 0
    */
-  defaultIndex?: number;
+  defaultIndex?: number
 
   /**
    * Additional label in tabs list when `items` is provided.
    */
-  label?: ReactNode;
+  label?: ReactNode
 }
 
 const TabsContext = createContext<{
-  items?: string[];
-  collection: CollectionKey[];
-} | null>(null);
+  items?: string[]
+  collection: CollectionKey[]
+} | null>(null)
 
 function useTabContext() {
-  const ctx = useContext(TabsContext);
-  if (!ctx) throw new Error('You must wrap your component in <Tabs>');
-  return ctx;
+  const ctx = useContext(TabsContext)
+  if (!ctx) throw new Error('You must wrap your component in <Tabs>')
+  return ctx
 }
 
 export function TabsList(props: React.ComponentPropsWithRef<typeof Unstyled.TabsList>) {
@@ -54,11 +54,11 @@ export function TabsList(props: React.ComponentPropsWithRef<typeof Unstyled.Tabs
     <Unstyled.TabsList
       {...props}
       className={cn(
-        'flex gap-3.5 text-fd-secondary-foreground overflow-x-auto px-4 not-prose',
+        'text-fd-secondary-foreground not-prose flex gap-3.5 overflow-x-auto px-4',
         props.className,
       )}
     />
-  );
+  )
 }
 
 export function TabsTrigger(props: React.ComponentPropsWithRef<typeof Unstyled.TabsTrigger>) {
@@ -66,11 +66,11 @@ export function TabsTrigger(props: React.ComponentPropsWithRef<typeof Unstyled.T
     <Unstyled.TabsTrigger
       {...props}
       className={cn(
-        'inline-flex items-center gap-2 whitespace-nowrap text-fd-muted-foreground border-b border-transparent py-2 text-sm font-medium transition-colors [&_svg]:size-4 hover:text-fd-accent-foreground disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-fd-primary data-[state=active]:text-fd-primary',
+        'text-fd-muted-foreground hover:text-fd-accent-foreground data-[state=active]:border-fd-primary data-[state=active]:text-fd-primary inline-flex items-center gap-2 border-b border-transparent py-2 text-sm font-medium whitespace-nowrap transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-4',
         props.className,
       )}
     />
-  );
+  )
 }
 
 export function Tabs({
@@ -82,26 +82,26 @@ export function Tabs({
   defaultValue = items ? escapeValue(items[defaultIndex]) : undefined,
   ...props
 }: TabsProps) {
-  const [value, setValue] = useState(defaultValue);
-  const collection = useMemo<CollectionKey[]>(() => [], []);
+  const [value, setValue] = useState(defaultValue)
+  const collection = useMemo<CollectionKey[]>(() => [], [])
 
   return (
     <Unstyled.Tabs
       ref={ref}
       className={cn(
-        'flex flex-col overflow-hidden rounded-xl border bg-fd-secondary my-4',
+        'bg-fd-secondary my-4 flex flex-col overflow-hidden rounded-xl border',
         className,
       )}
       value={value}
       onValueChange={(v: string) => {
-        if (items && !items.some((item) => escapeValue(item) === v)) return;
-        setValue(v);
+        if (items && !items.some((item) => escapeValue(item) === v)) return
+        setValue(v)
       }}
       {...props}
     >
       {items && (
         <TabsList>
-          {label && <span className="text-sm font-medium my-auto me-auto">{label}</span>}
+          {label && <span className="my-auto me-auto text-sm font-medium">{label}</span>}
           {items.map((item) => (
             <TabsTrigger key={item} value={escapeValue(item)}>
               {item}
@@ -113,32 +113,32 @@ export function Tabs({
         {props.children}
       </TabsContext.Provider>
     </Unstyled.Tabs>
-  );
+  )
 }
 
 export interface TabProps extends Omit<ComponentProps<typeof Unstyled.TabsContent>, 'value'> {
   /**
    * Value of tab, detect from index if unspecified.
    */
-  value?: string;
+  value?: string
 }
 
 export function Tab({ value, ...props }: TabProps) {
-  const { items } = useTabContext();
+  const { items } = useTabContext()
   const resolved =
     value ??
     // eslint-disable-next-line react-hooks/rules-of-hooks -- `value` is not supposed to change
-    items?.at(useCollectionIndex());
+    items?.at(useCollectionIndex())
   if (!resolved)
     throw new Error(
       'Failed to resolve tab `value`, please pass a `value` prop to the Tab component.',
-    );
+    )
 
   return (
     <TabsContent value={escapeValue(resolved)} {...props}>
       {props.children}
     </TabsContent>
-  );
+  )
 }
 
 export function TabsContent({
@@ -151,14 +151,14 @@ export function TabsContent({
       value={value}
       forceMount
       className={cn(
-        'p-4 text-[0.9375rem] bg-fd-background rounded-xl outline-none prose-no-margin data-[state=inactive]:hidden [&>figure:only-child]:-m-4 [&>figure:only-child]:border-none',
+        'bg-fd-background prose-no-margin rounded-xl p-4 text-[0.9375rem] outline-none data-[state=inactive]:hidden [&>figure:only-child]:-m-4 [&>figure:only-child]:border-none',
         className,
       )}
       {...props}
     >
       {props.children}
     </Unstyled.TabsContent>
-  );
+  )
 }
 
 /**
@@ -168,23 +168,23 @@ export function TabsContent({
  * This is supposed by work with pre-rendering & pure client-side rendering.
  */
 function useCollectionIndex() {
-  const key = useId();
-  const { collection } = useTabContext();
+  const key = useId()
+  const { collection } = useTabContext()
 
   useEffect(() => {
     return () => {
-      const idx = collection.indexOf(key);
-      if (idx !== -1) collection.splice(idx, 1);
-    };
-  }, [key, collection]);
+      const idx = collection.indexOf(key)
+      if (idx !== -1) collection.splice(idx, 1)
+    }
+  }, [key, collection])
 
-  if (!collection.includes(key)) collection.push(key);
-  return collection.indexOf(key);
+  if (!collection.includes(key)) collection.push(key)
+  return collection.indexOf(key)
 }
 
 /**
  * only escape whitespaces in values in simple mode
  */
 function escapeValue(v: string): string {
-  return v.toLowerCase().replace(/\s/, '-');
+  return v.toLowerCase().replace(/\s/, '-')
 }
